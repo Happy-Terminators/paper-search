a67726a4e2b1ae8ad0f0ccd536bc30263563ba9f
SongDaeSun <backgom07@gmail.com>
2019-10-26 17:56:25 +0900

    done basic

--- new.py
+++ new.py
@@ -1,19 +1,20 @@
 import sys
 import requests
 from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def get_citations(content):
-    out = 0
-    for char in range(0,len(content)):
-        if content[char:char+5] == '회 인용':
-            init = char                      
-            for end in range(init-6, init):
-                if content[end] == '>':
-                    break
-            out = content[end+1:init]
-            print(out)
-    return int(out)
-    
+    end = content.find('회 인용')
+    start = 0
+
+    for s in range(end-6, end):
+        if content[s] == '>':
+            start = s
+            break
+
+    return int(content[start + 1 : end])
+
 def get_year(content):
     for char in range(0,len(content)):
         if content[char] == '-':
@@ -22,34 +23,55 @@ def get_year(content):
         out = 0
     return int(out)
 
-def get_author(content):
-    for char in range(0,len(content)):
-        if content[char] == '-':
-            out = content[2:char-1]
-            break
-    return out
+keyword ='starcraft'
+start_year = 2017
+end_year = 2019
 
-keyword = 'starcraft'
-url = 'https://scholar.google.com/scholar?start='+str(1)+'&q='+keyword.replace(' ','+')
+url = 'https://scholar.google.co.kr/scholar?start='+ str(0) +'&q='+ keyword +'&hl=ko&as_sdt=0,5&as_ylo='+str(start_year)+'&as_yhi='+str(end_year)
 html = requests.get(url).text
 
-soup = BeautifulSoup(html,"html5lib")
-mydivs = soup.findAll("div", { "class" : "gs_r" })
+soup = BeautifulSoup(html,  "html.parser")
+mydivs = soup.find_all("div", class_="gs_ri")
+
 #print(mydivs[0])
+print(get_citations(str(mydivs[1])))
+print(get_year(str(mydivs[1].find_all("div", class_="gs_a"))))
 
-for div in mydivs:
+try:
+    print(mydivs[1].find('h3').find('a').get('href'))
+except: # catch *all* exceptions
+    print('Look manually at: https://scholar.google.com/scholar?start='+str(n)+'&q=non+intrusive+load+monitoring')
+        
+try:
+    print(mydivs[1].find('h3').find('a').text)
+except: 
+    print('Could not catch title')
+
+'''
+for n in range(0, number_of_results, 10):    
+    url = 'https://scholar.google.com/scholar?start='+str(n)+'&q='+keyword.replace(' ','+')
+    page = session.get(url)
+    c = page.content
     
-    try:
-        print(div.find('h3').find('a').get('href'))
-    except: # catch *all* exceptions
-        print('Look manually at: https://scholar.google.com/scholar?start='+str(1)+'&q=non+intrusive+load+monitoring')
-       
-    try:
-        print(div.find('h3').find('a').text)
-    except: 
-        print('Could not catch title')
-
-    print(get_citations(str(div.format_string)))
-    print(get_year(div.find('div',{'class' : 'gs_a'}).text))
-    print(get_author(div.find('div',{'class' : 'gs_a'}).text))
-    print()
+    # Create parser
+    soup = BeautifulSoup(c, 'html.parser')
+    
+    # Get stuff
+    mydivs = soup.findAll("div", { "class" : "gs_r" })
+    print(mydivs[0])
+ 
+    for div in mydivs:
+        try:
+            links.append(div.find('h3').find('a').get('href'))
+        except: # catch *all* exceptions
+            links.append('Look manually at: https://scholar.google.com/scholar?start='+str(n)+'&q=non+intrusive+load+monitoring')
+        
+        try:
+            title.append(div.find('h3').find('a').text)
+        except: 
+            title.append('Could not catch title')
+            
+        citations.append(get_citations(str(div.format_string)))
+        year.append(get_year(div.find('div',{'class' : 'gs_a'}).text))
+        author.append(get_author(div.find('div',{'class' : 'gs_a'}).text))
+'''
