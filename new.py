9aedc17234c393066fc1ba47ceee434d68590912
18송대선 <backgom07@gmail.com>
2019-10-28 19:31:33 +0900

    add save to csv function

--- new.py
+++ new.py
@@ -1,8 +1,29 @@
 import sys
 import requests
 from bs4 import BeautifulSoup
-import matplotlib.pyplot as plt
 import pandas as pd
+import time
+import random
+import os
+
+keyword ='starcraft'
+start_year = 2017
+end_year = 2019
+total_paper = 10000
+total_page = 1000
+maximum_paper = 20
+
+result_data = pd.DataFrame(columns=['Title', 'Link', 'Citations', 'Year'])
+
+basic_dir = os.getcwd()
+
+min_cit = 0
+
+title = ""
+links = ""
+citation = 0
+year = 0
+source = list()
 
 def get_citations(content):
     end = content.find('회 인용')
@@ -23,59 +44,91 @@ def get_year(content):
         out = 0
     return int(out)
 
-keyword ='starcraft'
-start_year = 2017
-end_year = 2019
-
-url = 'https://scholar.google.co.kr/scholar?start='+ str(0) +'&q='+ keyword +'&as_ylo='+str(start_year)+'&as_yhi='+str(end_year)
-html = requests.get(url).text
-
-print(html)
 
 
-'''
-soup = BeautifulSoup(html, "html.parser")
-mydivs = soup.find_all("div", class_="gs_ri")
+def extract(html, is_text, maximum_n, data, min_citation):
+    soup = BeautifulSoup(html, "html5lib")
+    papers = soup.find_all("div", class_="gs_ri")
 
-#print(mydivs[0])
-print(get_citations(str(mydivs[1])))
-print(get_year(str(mydivs[1].find_all("div", class_="gs_a"))))
+    if papers:
+        for paper in papers:
 
-try:
-    print(mydivs[1].find('h3').find('a').get('href'))
-except: # catch *all* exceptions
-    print('Look manually at: https://scholar.google.com/scholar?start='+str(n)+'&q=non+intrusive+load+monitoring')
-        
-try:
-    print(mydivs[1].find('h3').find('a').text)
-except: 
-    print('Could not catch title')
+            citation = (get_citations(str(paper)))
 
+            if data.shape[0] < maximum_paper:
+                try:
+                    title = (paper.find('h3').find('a').text)
+                except: 
+                    title = ('Could not catch title')
 
-for n in range(0, number_of_results, 10):    
-    url = 'https://scholar.google.com/scholar?start='+str(n)+'&q='+keyword.replace(' ','+')
-    page = session.get(url)
-    c = page.content
-    
-    # Create parser
-    soup = BeautifulSoup(c, 'html.parser')
-    
-    # Get stuff
-    mydivs = soup.findAll("div", { "class" : "gs_r" })
-    print(mydivs[0])
- 
-    for div in mydivs:
-        try:
-            links.append(div.find('h3').find('a').get('href'))
-        except: # catch *all* exceptions
-            links.append('Look manually at: https://scholar.google.com/scholar?start='+str(n)+'&q=non+intrusive+load+monitoring')
-        
-        try:
-            title.append(div.find('h3').find('a').text)
-        except: 
-            title.append('Could not catch title')
+                try:
+                    links = (paper.find('h3').find('a').get('href'))
+                except: # catch *all* exceptions
+                    links = ('Look manually at: https://scholar.google.co.kr/scholar?start='+ str(page_n * 10) +'&q='+ keyword +'&as_ylo='+str(start_year)+'&as_yhi='+str(end_year))
             
-        citations.append(get_citations(str(div.format_string)))
-        year.append(get_year(div.find('div',{'class' : 'gs_a'}).text))
-        author.append(get_author(div.find('div',{'class' : 'gs_a'}).text))
-'''
+                year = (get_year(str(paper.find_all("div", class_="gs_a"))))
+
+                data = data.append({'Title': title, 'Link': links, 'Citations': citation, 'Year': year}, ignore_index=True)
+                data = data.sort_values('Citations', ascending=False)
+                data = data.reset_index(drop=True)
+                min_citation = data.loc[data.shape[0]-1, 'Citations']
+
+            elif citation > min_citation:
+                try:
+                    title = (paper.find('h3').find('a').text)
+                except: 
+                    title = ('Could not catch title')
+
+                try:
+                    links = (paper.find('h3').find('a').get('href'))
+                except: # catch *all* exceptions
+                    links = ('Look manually at: https://scholar.google.co.kr/scholar?start='+ str(page_n * 10) +'&q='+ keyword +'&as_ylo='+str(start_year)+'&as_yhi='+str(end_year))
+            
+                year = (get_year(str(paper.find_all("div", class_="gs_a"))))
+
+                data = data.append({'Title': title, 'Link': links, 'Citations': citation, 'Year': year}, ignore_index=True)
+                data = data.sort_values('Citations', ascending=False)
+                data = data.reset_index(drop=True)
+                data = data.drop(maximum_paper)
+
+                min_citation = data.loc[maximum_paper-1, 'Citations']
+
+        if is_text:
+            source.append(str(page_n) + "~" + str(page_n * 10 - 1) + ": txt")
+        else:
+            source.append(str(page_n) + "~" + str(page_n * 10 - 1) + ": internet")
+    else:
+        source.append(str(page_n) + "~" + str(page_n * 10 - 1) + ": manual")
+
+    return data, min_citation
+
+
+
+
+for page_n in range(0, 3):
+    
+    file_dir = basic_dir + "\\htmls\\page" + str(page_n) + ".txt"
+
+    if os.path.isfile(file_dir):
+        f = open(file_dir, 'r')
+        html = f.read()
+        f.close()
+
+        result_data, min_cit = extract(html, True, maximum_paper, result_data, min_cit)
+    else:
+        url = 'https://scholar.google.co.kr/scholar?start='+ str(page_n * 10) +'&q='+ keyword +'&as_ylo='+str(start_year)+'&as_yhi='+str(end_year)
+        html = requests.get(url).text
+
+        f = open(file_dir, 'w')
+        f.write(html)
+        f.close()
+
+        result_data, min_cit = extract(html, False, maximum_paper, result_data, min_cit)
+
+        rand_value = random.randint(1, 10)
+        time.sleep(rand_value)
+
+csv_dir = basic_dir + "\\csvs\\" + keyword + ".csv"
+
+result_data.to_csv(csv_dir, encoding='utf-8')
+print(source)
